631df9d27bfbbb4af5b79b57ef760009e8a27155
veer260 <57021124+veer260@users.noreply.github.com>
2023-03-28 18:09:16 +0530

    resolved gig page bug

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const newRequest = axios.create({
   baseURL: "https://fiverrapp.onrender.com/api/",
+  // baseURL: "http://localhost:8800/api/",
   withCredentials: true,
 });
 
